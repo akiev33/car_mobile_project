fdf23d1f79e7fe5bca238ec3abbfcd79d41f5d32
akiev33 <71421550+akiev33@users.noreply.github.com>
2022-11-13 00:58:37 +0600

    update code "loading screen"

--- lib/screens/loadingScreen.dart
+++ lib/screens/loadingScreen.dart
@@ -1,6 +1,5 @@
 import 'package:car_mobile_project/resources/resources.dart';
 import 'package:car_mobile_project/theme/colors.dart';
-import 'package:car_mobile_project/theme/text_font.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,64 +9,89 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(Images.carScreen),
-          Column(
-            children: [
-              const SizedBox(height: 240),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 150),
-                child: SizedBox(
-                  height: 60,
-                  width: 145,
-                  child: Image.asset(Images.titleInMainScreen),
-                ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Images.carScreen),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 256),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 150),
+              child: SizedBox(
+                height: 60,
+                width: 145,
+                child: Image.asset(Images.titleInMainScreen),
               ),
-              const SizedBox(height: 340),
-              Text(
-                'Rent your dream car from the\nBest Company',
-                textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 332),
+            SizedBox(
+              height: 35,
+              child: Text(
+                'Rent your dream car from the',
                 style: GoogleFonts.hind(
-                  textStyle: AppTextStyle.hindW600Size26,
+                  textStyle: TextStyle(
+                    color: AppColors.hindF6F6F6.withOpacity(0.81),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 28,
+                  ),
                 ),
               ),
-              const SizedBox(height: 70),
-              SizedBox(
-                height: 55,
-                width: 245, //243
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        AppColors.inconsolataC64949),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
+            ),
+            Text(
+              'Best Company',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.hind(
+                textStyle: TextStyle(
+                  color: AppColors.hindF6F6F6.withOpacity(0.81),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const SizedBox(height: 67),
+            SizedBox(
+              height: 55,
+              width: 260, //243
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      AppColors.inconsolataC64949),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Get Started',
-                        style: GoogleFonts.inconsolata(
-                          textStyle: AppTextStyle.hindW800Size20,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Get Started',
+                      style: GoogleFonts.inconsolata(
+                        textStyle: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 21,
+                          color: AppColors.hindFFFFFF,
                         ),
                       ),
-                      const SizedBox(width: 10),
-                      Image.asset(
-                        Images.vector,
-                        scale: 4,
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 8),
+                    Image.asset(
+                      Images.vector,
+                      scale: 4,
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
